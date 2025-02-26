f86cd771e82e3e66c819c430a0ec3d0a9dc3322f
Kimpembie2 <119584034+Kimpembie2@users.noreply.github.com>
2025-02-26 12:50:35 +0000

    about us

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from blueprints.auth import auth_bp
 from blueprints.home import home_bp
+from blueprints.about_us import aboutus_bp
 
 app = Flask(__name__)
 app.secret_key = "chiavesessione"
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(home_bp)
+app.register_blueprint(aboutus_bp)
 
 if __name__ == "__main__":
     app.run(debug = True, port=5001)
