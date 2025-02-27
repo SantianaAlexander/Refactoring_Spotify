6f7f3209d54624dd8a7db0c1e740a472bba451c8
SantianaAlexander <149154929+SantianaAlexander@users.noreply.github.com>
2025-02-27 13:07:23 +0000

    Pagina visualizza brani personali fatta

--- blueprints/playlist.py
+++ blueprints/playlist.py
@@ -11,34 +11,35 @@ def get_spotify_client():
     if token_info:
         return spotipy.Spotify(auth=token_info.get("access_token"))
     return spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-        client_id="af4cae999c184ad7b760fb8c51b60d60",
-        client_secret="9d1c008a6aaa4a969b178224406d5a73"
+        client_id="af4cae999c184ad7b760fb8c51b60d60",  
+        client_secret="9d1c008a6aaa4a969b178224406d5a73" 
     ))
 
 @playlist_bp.route('/playlist')
 def playlist():
-    profile_pic_url = None
     token_info = session.get('token_info', None)
     if not token_info:
-        return redirect(url_for('login'))
-    sp = spotipy.Spotify(auth=token_info['access_token']) 
-    if isinstance(sp, spotipy.Spotify) and session.get("token_info"):
-        try:
-            if user_info.get("images"):
-                profile_pic_url = user_info["images"][0]["url"]
-        except Exception:
-            pass
-    user_info = sp.current_user()
-    print(user_info)
-    playlists = sp.current_user_playlists() 
-    playlists_info = playlists['items'] 
-    return render_template('playlist.html', user_info=user_info, playlists=playlists_info, profile_pic_url = profile_pic_url)
+        return redirect(url_for('auth.login')) 
 
-@playlist_bp.route('/playlist/<playlist_id>')
-def visualizza_playlist(playlist_id):
-    token_info = session.get('token_info', None)
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+    sp = get_spotify_client() 
+    playlists = sp.current_user_playlists()
+    playlists_info = playlists['items']  
+
+    profile_pic_url = None
     user_info = sp.current_user()
-    brani_playlist = sp.playlist_items(playlist_id)
-    brano_singolo = brani_playlist['items']
-    return render_template('playlist.html', brani = brano_singolo, user_info = user_info)
+    if user_info.get("images"):
+        profile_pic_url = user_info["images"][0]["url"]
+
+    return render_template('playlist.html', playlists=playlists_info, profile_pic_url=profile_pic_url)
+
+@playlist_bp.route('/playlist_details/<playlist_id>')
+def playlist_details(playlist_id):
+    token_info = session.get('token_info', None)
+    if not token_info:
+        return redirect(url_for('auth.login'))  
+
+    sp = get_spotify_client() 
+    results = sp.playlist_tracks(playlist_id)  
+    tracks = results['items'] 
+
+    return render_template('playlist_details.html', tracks=tracks)
